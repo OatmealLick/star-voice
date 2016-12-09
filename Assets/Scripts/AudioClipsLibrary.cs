5a816e0c70663516d33da2a82364b5a111c79fe9
kenjik <kenjikacper@gmail.com>
2016-12-10 00:53:37 +0100

    Audio clips managment

---
+++ Assets/Scripts/AudioClipsLibrary.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioClipsLibrary : MonoBehaviour {
+
+    public Dictionary<string, AudioClip> audioClips;
+	// Use this for initialization
+	void Start () {
+        audioClips.Add("forest", (AudioClip)Resources.Load("forest"));
+        audioClips.Add("fire", (AudioClip)Resources.Load("fire"));
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
