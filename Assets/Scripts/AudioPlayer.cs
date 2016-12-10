2b8217678ff4d6d61faf708746af10116a40a8b2
kenjik <kenjikacper@gmail.com>
2016-12-10 01:23:44 +0100

    Revert "Sound is working quite nicely I would say m'lord."
    This reverts commit 7bbefc1551bcba4ea94b716e45152a548f6b9e79.

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour {
+
+    AudioSource source;
+    AudioClipsLibrary library;
+	// Use this for initialization
+	void Start () {
+        source = GetComponent<AudioSource>();
+        library = new AudioClipsLibrary();
+        Forest();
+	}
+	
+    public void Forest(){
+        AudioClip clip;
+        library.audioClips.TryGetValue("forest", out clip);
+        source.clip = clip;
+        source.Play();
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
