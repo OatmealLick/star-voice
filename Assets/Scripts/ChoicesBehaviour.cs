01e04ce269532944222b2051f529d839880d0153
OatmealLick <scigalukasz@gmail.com>
2016-12-10 01:01:10 +0100

    Initial commit

---
+++ Assets/Scripts/ChoicesBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChoicesBehaviour : MonoBehaviour {
+	
+	private GameObject[] choices;
+	private float widthScale = 0.8f;
+	private int choiceHeightInPixels = 100;
+
+	public string[] choiceTexts;
+
+	// Use this for initialization
+	void Start () {
+		// load choice GameObject to enable instantiating
+		GameObject choice = (GameObject)Resources.Load ("Choice");
+
+		Rect rect = gameObject.GetComponent<RectTransform> ().rect;
+		rect.width = widthScale * gameObject.transform.parent.GetComponent<RectTransform> ().rect.width;
+		rect.height = choiceTexts.Length * choiceHeightInPixels;
+
+		// initialise choices array
+		choices = new GameObject[choiceTexts.Length];
+
+		Vector3 pos = gameObject.transform.position;
+
+		// for every choice option instantiate GO, set transform and proper text
+		for (int i = 0; i < choices.Length; i++) {
+			choices [i] = Instantiate (choice, pos + new Vector3(0, -i*choiceHeightInPixels, 0), transform.rotation);
+			choices [i].transform.SetParent (gameObject.transform, false);
+
+			// TODO set transform and rotation
+			choices [i].GetComponentInChildren<Text>().text=choiceTexts[i];
+
+		}
+	}
+}
---
+++ Assets/Scripts/TextAppear.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TextAppear : MonoBehaviour {
+
+	private Text text;
+	public string stringToDisplay = "Where am I?";
+	public float timeToDisplay = -1;
+	public float timeToStay = 2f;
+
+	private float readerSpeed = 13;
+	// Use this for initialization
+	void Start () {
+		text = gameObject.GetComponent<Text> ();
+		StartCoroutine (AnimateText(stringToDisplay));
+
+
+		if (timeToDisplay == -1)
+			timeToDisplay = stringToDisplay.Length / readerSpeed;
+	}
+
+	IEnumerator AnimateText (string finalString) {
+		float waitSec = timeToDisplay / stringToDisplay.Length;
+		int i = 0;
+		string str = string.Empty;
+		while(i < stringToDisplay.Length){
+			str += stringToDisplay[i++];
+			text.text = str;
+			yield return new WaitForSeconds(waitSec);
+
+		}
+		yield return new WaitForSeconds (timeToStay);
+		text.text = string.Empty;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
