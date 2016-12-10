3147e669a40c418dd3687dd5104fc44da1ef3e65
OatmealLick <scigalukasz@gmail.com>
2016-12-10 02:12:27 +0100

    final highlights

--- Assets/Scripts/Plot.cs
+++ Assets/Scripts/Plot.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Plot : MonoBehaviour {
+	List<int> plotDecisions = new List<int>;
+	int plot = 0;
 
 	// Use this for initialization
 	void Start () {
